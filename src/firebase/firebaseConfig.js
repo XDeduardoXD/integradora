5762bf5cade0b9075f459d624f20fc03e179bf2c
XDeduardoXD <95115682+XDeduardoXD@users.noreply.github.com>
2022-03-30 20:23:02 -0600

    cambio de firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,12 +4,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDmBHnqGylgWjrBzQZ9U8Q7zts7dMbqIg",
-  authDomain: "iotapp-a1ead.firebaseapp.com",
-  projectId: "iotapp-a1ead",
-  storageBucket: "iotapp-a1ead.appspot.com",
-  messagingSenderId: "466918626717",
-  appId: "1:466918626717:web:c7cd73292f9b8a8aeb4b74"
+  apiKey: "AIzaSyAdtTKKIDyoS3qabq1VLY8L9vZl2eujysw",
+  authDomain: "selva-pets-2.firebaseapp.com",
+  projectId: "selva-pets-2",
+  storageBucket: "selva-pets-2.appspot.com",
+  messagingSenderId: "745922927691",
+  appId: "1:745922927691:web:70afc4de37596dc244c1ce"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore()
